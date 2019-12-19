4f37237cf29a233926608c8d38c5ab812404b398
rahmansaleh7 <rahman.saleh7@gmail.com>
2019-12-19 13:07:57 +0700

    Menambahkan XML dan py

--- models/models.py
+++ models/models.py
@@ -1,6 +1,62 @@
 # -*- coding: utf-8 -*-
 
-from odoo import models, fields, api
+from odoo import models, fields, api, _
+
+class Surat(models.Model):
+	_name = 'vit_eoffice.surat'
+
+	nosurat = fields.Char(string="No Surat", required=True, )
+	name = fields.Char(string="Perihal", required=True, )
+	tanggal = fields.Date(string="Tanggal", required=True, )
+	dari	= fields.Char(string="Dari", required=True, )
+	klasifikasisurat = fields.Many2one(comodel_name="vit_eoffice.klasifikasisurat",
+									string="Klasifikasi Surat", 
+									required="True")
+	templatesurat = fields.Many2one(comodel_name="vit_eoffice.templatesurat",
+									string="Template Surat", 
+									required="True")
+	isisurat = fields.Text(string="Isi Surat", )
+	history = fields.Char(string="History", )
+
+
+class Klasifikasisurat(models.Model):
+	_name ='vit_eoffice.klasifikasisurat'
+
+	kodesurat = fields.Char(string="Kode Surat", required=True, )
+	name = fields.Char(string="Nama", required=True, )
+
+
+class Templatesurat(models.Model):
+	_name = 'vit_eoffice.templatesurat'
+
+	kodetemp = fields.Char(string="Kode", required=True, )
+	name = fields.Char(string="Nama", required=True, )
+	tempsurat = fields.Text(string="Template Surat", )
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 # class vit_eoffice(models.Model):
 #     _name = 'vit_eoffice.vit_eoffice'
